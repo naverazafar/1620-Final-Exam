292fd01039952e92c4e8daec01bed31f839e450d
Navera <naverazafar23@gmail.com>
2021-12-10 12:39:55 -0800

    add feature that logs info in form to the console

--- JS/script.js
+++ JS/script.js
@@ -3,3 +3,18 @@ const themeSwitch = document.querySelector('input');
 themeSwitch.addEventListener('change', () => {
   document.body.classList.toggle('dark-theme');
 });
+
+function logSubmit(evt) {
+    evt.preventDefault()
+    objAdd = {
+            name: document.getElementById('name').value,
+            email: document.getElementById('email').value,
+            message: document.getElementById('msg').value,
+    }
+    
+    console.log("Object", objAdd)
+}
+
+const submitButton = document.querySelector('.contact button')
+
+submitButton.addEventListener('click', logSubmit)
